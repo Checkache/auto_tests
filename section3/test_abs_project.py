8e9af3e94c605af21d5195dc2c1ce5a8d418f40d
unknown <gromov2704@gmail.com>
2025-07-20 17:33:32 +0300

    changed lesson6_step10.py and test_abs_project.py

--- section3/test_abs_project.py
+++ section3/test_abs_project.py
@@ -1,6 +1,41 @@
-def test_abs1():
-    assert abs(-42) == 42, "Should be absolute value of a number" 
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+class TestRegistration(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+        time.sleep(1)
+        self.browser.quit()
+
+    def fill_form_and_submit(self, url):
+        browser = self.browser
+        browser.get(url)
+
+        # Заполняем обязательные поля
+        browser.find_element(By.CSS_SELECTOR, ".first_block .form-control.first").send_keys("Ivan")
+        browser.find_element(By.CSS_SELECTOR, ".first_block .form-control.second").send_keys("Petrov")
+        browser.find_element(By.CSS_SELECTOR, ".first_block .form-control.third").send_keys("test@example.com")
+
+        # Отправляем форму
+        browser.find_element(By.CSS_SELECTOR, "button.btn").click()
+
+        # Ждём загрузки новой страницы
+        time.sleep(1)
+
+        # Ищем элемент с текстом
+        return browser.find_element(By.TAG_NAME, "h1").text
+
+    def test_registration1(self):
+        result_text = self.fill_form_and_submit("http://suninjuly.github.io/registration1.html")
+        self.assertEqual(result_text, "Congratulations! You have successfully registered!", "registration1 failed")
+
+    def test_registration2(self):
+        result_text = self.fill_form_and_submit("http://suninjuly.github.io/registration2.html")
+        self.assertEqual(result_text, "Congratulations! You have successfully registered!", "registration2 failed")
 
 if __name__ == "__main__":
-    test_abs1()
-    print("All tests passed!")
+    unittest.main()
